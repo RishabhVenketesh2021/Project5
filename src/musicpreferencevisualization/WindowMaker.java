ede2c0fe7081832770bf05394697a1ae4a9801c3
Jerin Manalel <jerinm@vt.edu>
2018-04-09 00:25:24 -0400

    added_windowMaker_class
    added the window class

---
+++ src/musicpreferencevisualization/WindowMaker.java
@@ -0,0 +1,25 @@
+package musicpreferencevisualization;
+
+import java.awt.Color;
+import CS2114.Window;
+import CS2114.Button;
+import CS2114.Shape;
+import CS2114.WindowSide;
+
+public class WindowMaker {
+
+    
+    private Window window;
+    private Shape shape;
+    private Button prev;
+    private Button sortArtist;
+    private Button sortName;
+    private Button sortYear;
+    private Button sortGenre;
+    private Button next;
+    private Button repHobby;
+    private Button repMajor;
+    private Button repRegion;
+    
+    
+}
